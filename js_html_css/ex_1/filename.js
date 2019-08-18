559b77c76cd1b7fcfc65f4c4177be52d8dfbd988
diego-gm <diego.gm@globant.com>
2019-08-18 13:33:00 -0300

    log in console result of longest string

--- js_html_css/ex_1/filename.js
+++ js_html_css/ex_1/filename.js
@@ -8,11 +8,15 @@
 
 function longestString(strings) {
     let s = '';
-    for (let string in strings) {
+    for (let string of strings) {
         if (string.length > s.length) {
             s = string;
         }
     }
+    console.log(s)
     return s;
     // your code goes here!
     }
+
+const str_arr = [ 'cat', 'rabbit', 'elephant' ];
+longestString(str_arr);
